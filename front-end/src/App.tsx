201415c9660952d788d22b90e77061dac74146ae
Jesus Medina Casas <chuytec15@gmail.com>
2026-02-16 11:36:06 -0600

    feat: agregar configuracion de docker y ajustes de frontend

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,8 @@
-
 import './App.css'
 import { AppLayout } from "@/ui/layout/AppLayout";
 import LandingPage from "@/modules/landing/pages/LandingPage"
-import { AppRouter } from './app/routes/AppRouter';
+// Ponemos las llaves porque NO es una exportación default
+import { AppRouter } from "@/app/routes/AppRouter"; 
 
 function App() {
   return (
